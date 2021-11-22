528e9d018dd455c3f12bed9ee90deb7db6445f94
Maya Sharma <mayasharma95@outlook.com>
2021-11-22 19:28:23 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JyhUKDDUfYNRp9E51j9fO3uFKjv1OcXyFLndBV3trbVlRxNqZUVqInJZuVm46xNw6bJO2bCGKmNVqNU001Ao21K00R1AkkNQt',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
